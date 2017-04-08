9fbd773d8beb197ce8f740ff3461ccd2ca90fbb6
SheepAvenger <robert.mccook.elm@gmail.com>
2017-04-08 11:57:21 -0700

    Add files via upload

---
+++ hash.h
@@ -0,0 +1,102 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include "stdlib.h"
+
+struct hash *symbolTable = NULL;
+int eleCount = 0;
+
+long long hash(char* string, int size);
+void insertToHash(char *string, int scope, int size);
+void display(int size);
+void setSize(int size);
+
+struct node
+{
+    char* string;
+    int scope;
+    struct node* next;
+
+};
+struct hash
+{
+    struct node *head;
+    int count;
+    void (*insertToHash)(char*, int, int);
+    void (*display)(int);
+    void (*setSize)(int);
+    int (*getSize)();
+};
+
+ struct node * createNode(char *s, int scope)
+ {
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->scope = scope;
+    newnode->string = malloc(256);
+    strcpy(newnode->string, s);
+    newnode->next = NULL;
+    return newnode;
+  }
+
+long long hash(char* string, int size)
+{
+    long long key;
+
+	for (key = 1; *string;)
+	{
+		key = (key*(*string++)) % size;
+	}
+	return (key < 0 ? -key : key);
+}
+
+void setSize(int size)
+{
+   symbolTable = (struct hash *)calloc(size, sizeof (struct hash));
+}
+
+
+ void insertToHash(char *string, int scope, int size)
+{
+    long long hashIndex = hash(string,size);
+    struct node *newnode =  createNode(string, scope);
+    if (!symbolTable[hashIndex].head)
+        {
+            symbolTable[hashIndex].head = newnode;
+            symbolTable[hashIndex].count = 1;
+            return;
+        }
+
+    newnode->next = (symbolTable[hashIndex].head);
+    symbolTable[hashIndex].head = newnode;
+    symbolTable[hashIndex].count++;
+    eleCount++;
+    return;
+  }
+
+void display(int size)
+{
+    struct node *myNode;
+    int i;
+    for (i = 0; i < size; i++)
+    {
+
+        if (symbolTable[i].head != NULL)
+        {
+            myNode = symbolTable[i].head;
+             printf("\nData at index %d in Symbol Table:\n", i);
+            printf("String     Scope\n");
+            printf("----------------\n");
+            while (myNode != NULL)
+            {
+                printf("%-15s", myNode->string);
+                printf("%i\n", myNode->scope);
+                myNode = myNode->next;
+            }
+        }
+
+    }
+    return;
+  }
+
+#endif
---
+++ source.c
@@ -0,0 +1,54 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+#include "hash.h"
+
+int prime(int p);
+
+int main()
+{
+    FILE *fp;
+    long int size;
+    int i, scope = 0;
+    char* string = malloc(256);
+    fp = fopen("test.txt", "r");
+    fseek(fp, 1, SEEK_END);
+   size = ftell(fp) / 4;
+    printf("%i size\n", size);
+    i = size -1;
+    for(i; ; i--)
+    {
+        if(prime(i))
+            break;
+    }
+    rewind(fp);
+    struct hash symbolTable = {NULL, 0, insertToHash, display, setSize};
+    symbolTable.setSize(i);
+    while(fscanf(fp,"%s", string)>0)
+    {
+        if(strcmp(string, "{") == 0)
+            scope++;
+        else if(strcmp(string, "}") == 0)
+            scope--;
+        else
+            symbolTable.insertToHash(string, scope, i);
+        string = malloc(256);
+    }
+    symbolTable.display(i);
+   system("pause");
+   free(string);
+   return 0;
+}
+
+//check for prime numbers
+int prime(int p)
+{
+    int i;
+    for(i = 2; p%i != 0; i++);
+
+        if(p==i)
+            return 1;
+        else
+            return 0;
+
+}
