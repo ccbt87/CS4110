494f9dbbe13e2baaf8e402664c682419d251be35
SheepAvenger <robert.mccook.elm@gmail.com>
2017-04-10 23:12:22 -0700

    Add files via upload

--- Stack.h
+++ Stack.h
@@ -8,48 +8,66 @@
 void push(int);
 void pop();
 void printStack(char* name);
+void create(int);
 
 struct block
 {
     int scope;
     struct block* next;
+
+};
+
+struct stack
+{
+    int size;
+    struct block* head;
     void (*push)(int id);
     void (*pop)();
-    void (*printStack)(char*)
+    void (*printStack)(char*);
+    void (*create)(int);
 
-}*head;
+};
+
+struct stack* _stack = NULL;
+
+void create(int size)
+{
+    _stack = (struct stack *)calloc(size, sizeof (struct stack));
+}
 
 void push(int id)
 {
     struct block *newBlock;
-    if(!head)
+    if(!_stack->head)
     {
-        head = (struct block *)malloc(1*sizeof(struct block));
-        head->scope = id;
-        head->next = NULL;
+        newBlock = (struct block *)malloc(1*sizeof(struct block));
+        newBlock->scope = id;
+        newBlock->next = NULL;
+        _stack->head = newBlock;
         return;
     }
     newBlock = (struct block *)malloc(1*sizeof(struct block));
-    newBlock->next = head;
+    newBlock->next = _stack->head;
     newBlock->scope = id;
-    head = newBlock;
+    _stack->head = newBlock;
+
 }
 
 void pop()
 {
     struct block* myTemp;
-    myTemp = head->next;
-    free(head);
-    head = myTemp;
+    myTemp = _stack->head->next;
+    free(_stack->head);
+    _stack->head = myTemp;
 }
 
 void printStack(char* name)
 {
     struct block* myBlock;
-    if(head)
+    if(_stack->head)
     {
         printf("%s Contents\n", name);
-        myBlock = head;
+        myBlock = _stack->head;
         while(myBlock != NULL)
         {
             printf("Block ID: %i\n", myBlock->scope);
